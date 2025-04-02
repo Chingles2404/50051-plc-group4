7468c5c1ff0a2894bf1e93e05f78e861d115393f
Sean Lim <sean123lim@gmail.com>
2025-04-02 18:03:06 +0800

    merged process_bitmap branch into main

--- src/process_bitmap/main.c
+++ src/process_bitmap/main.c
@@ -4,20 +4,25 @@
 #include "utils.h"
 
 int main(int argc, char **argv) {
+    const char *filename;
+    FILE *fp;
+    FILEHEADER fileHeader;
+    INFOHEADER infoHeader;
+    unsigned char *pixelData;
+    int i;
+
     if (argc < 2) {
         printf("Usage: %s <bitmap_file>\n", argv[0]);
         return EXIT_FAILURE;
     }
 
-    const char *filename = argv[1];
-    FILE *fp = openBitmapFile(filename);
+    filename = argv[1];
+    fp = openBitmapFile(filename);
     if (fp == NULL) {
         perror("Failed to open file");
         return EXIT_FAILURE;
     }
 
-    FILEHEADER fileHeader;
-    INFOHEADER infoHeader;
     readBitmapHeaders(fp, &fileHeader, &infoHeader);
     if (!validateBitmap(&fileHeader)) {
         printf("File is not a valid BMP.\n");
@@ -28,8 +33,9 @@ int main(int argc, char **argv) {
     printf("Bitmap dimensions: %dx%d\n", infoHeader.width, infoHeader.height);
     printf("Bits per pixel: %d\n", infoHeader.bits);
 
-    unsigned char *pixelData = NULL;
+    pixelData = NULL;
     readPixelData(fp, &infoHeader, &pixelData);
+
     if (pixelData == NULL) {
         printf("Failed to read pixel data.\n");
         fclose(fp);
@@ -38,7 +44,7 @@ int main(int argc, char **argv) {
 
     /* I'm just testing here */
     printf("First 10 pixels (grayscale values): ");
-    for (int i = 0; i < 10 && i < infoHeader.width * infoHeader.height; i++) {
+    for (i = 0; i < 10 && i < infoHeader.width * infoHeader.height; i++) {
         printf("%d ", pixelData[i]);
     }
     printf("\n");
